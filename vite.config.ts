14f8d19683d892c7d2f0f051a4778d13b3b8a453
Anant Trivedi <theananttrivedi@gmail.com>
2024-10-01 19:05:27 +0530

    Added PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,31 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Anant Corider Assignment",
+        short_name: "AT Assignment",
+        description: "An assignment which had to be submitted for corider",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 });
